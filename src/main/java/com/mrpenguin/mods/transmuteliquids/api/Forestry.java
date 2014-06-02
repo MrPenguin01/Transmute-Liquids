2f8de75b5f6c4d0ba20ba101cfd255a0b8004877
MrPenguin01 <leahy.justin01@gmail.com>
2014-06-02 06:36:22 -0400

    Adding Seed Oil and Finished Juice

--- src/main/java/com/mrpenguin/mods/transmuteliquids/api/Forestry.java
+++ src/main/java/com/mrpenguin/mods/transmuteliquids/api/Forestry.java
@@ -20,7 +20,8 @@ public class Forestry {
 	private static final String canHoneyText = "After using bees to generate some honey for you, you learn the uses of honey. Afterwards, you want to find out if it's possible to create honey after making water from aspects.";
 	private static final String canBiomassText = "While working with saplings you discovered a new fuel source and can mass produce this fuel using saplings. After many unsuccessful attemps you finally have created your first millibuckets of Biomass from aspects.";
 	private static final String canBiofuelText = "You finally figured out that you can refine Biomass for a more efficient fuel source that can also be mass produced. You decide to see if you can create it from aspects also, and you were very successful.";
-   private static final String canJuiceText = "After making honey from aspects, you work on some fruits and create some Juice. Numerous failed attempts later you finally create some Juice from aspects.";
+    private static final String canJuiceText = "After making honey from aspects, you work on some fruits and create some Juice. Numerous failed attempts later you finally create some Juice from aspects.";
+    private static final String canSeedOilText = "";
 	
 	public static CrucibleRecipe potHoneyRecipe;
 	
@@ -28,21 +29,24 @@ public class Forestry {
 	public static CrucibleRecipe waxCapsuleHoneyRecipe;
 	public static CrucibleRecipe waxCapsuleBiofuelRecipe;
 	public static CrucibleRecipe waxCapsuleBiomassRecipe;
-   public static CrucibleRecipe waxCapsuleJuiceRecipe;
+    public static CrucibleRecipe waxCapsuleJuiceRecipe;
+    public static CrucibleRecipe waxCapsuleSeedOilRecipe;
 	
 	public static CrucibleRecipe refractoryWaterRecipe;
 	public static CrucibleRecipe refractoryLavaRecipe;
 	public static CrucibleRecipe refractoryHoneyRecipe;
 	public static CrucibleRecipe refractoryBiofuelRecipe;
 	public static CrucibleRecipe refractoryBiomassRecipe;
-   public static CrucibleRecipe refractoryJuiceRecipe;
+    public static CrucibleRecipe refractoryJuiceRecipe;
+    public static CrucibleRecipe refractorySeedOilRecipe;
 	
 	public static CrucibleRecipe canWaterRecipe;
 	public static CrucibleRecipe canLavaRecipe;
 	public static CrucibleRecipe canHoneyRecipe;
 	public static CrucibleRecipe canBiofuelRecipe;
 	public static CrucibleRecipe canBiomassRecipe;
-   public static CrucibleRecipe canJuiceRecipe;
+    public static CrucibleRecipe canJuiceRecipe;
+    public static CrucibleRecipe canSeedOilRecipe;
 	
 	private static AspectList canWaterAspectList = new AspectList().add(Aspect.WATER, 4).add(Aspect.EXCHANGE, 2);
 	private static AspectList canLavaAspectList = new AspectList().add(Aspect.FIRE, 4).add(Aspect.EXCHANGE, 2);
@@ -50,9 +54,10 @@ public class Forestry {
 	private static AspectList potHoneyAspectList = new AspectList().add(Aspect.CROP, 2).add(Aspect.EXCHANGE, 2);
 	
 	private static AspectList canHoneyAspectList = new AspectList().add(Aspect.CROP, 4).add(Aspect.EXCHANGE, 2);
-	private static AspectList canBiomassAspectList = new AspectList().add(Aspect.PLANT, 4).add(Aspect.EXCHANGE, 2);
-	private static AspectList canBiofuelAspectList = new AspectList().add(Aspect.PLANT, 4).add(Aspect.EXCHANGE, 2).add(Aspect.TOOL, 2);
-   private static AspectList canJuiceAspectList = new AspectList().add(Aspect.CROP, 4).add(Aspect.EXCHANGE, 2);
+	private static AspectList canBiomassAspectList = new AspectList().add(Aspect.PLANT, 4).add(Aspect.EXCHANGE, 2).add(Aspect.TOOL, 2);
+	private static AspectList canBiofuelAspectList = new AspectList().add(Aspect.PLANT, 4).add(Aspect.EXCHANGE, 2).add(Aspect.TOOL, 4);
+    private static AspectList canJuiceAspectList = new AspectList().add(Aspect.CROP, 4).add(Aspect.EXCHANGE, 2);
+    private static AspectList canSeedOilAspectList = new AspectList().add(Aspect.PLANT, 4).add(Aspect.EXCHANGE, 2);
 	
 	public static void crucibleRecipes() {
 		
@@ -62,18 +67,21 @@ public class Forestry {
 		waxCapsuleHoneyRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Honey", ForestryItem.waxCapsuleHoney.getItemStack(1), ForestryItem.waxCapsule.getItemStack(1), canHoneyAspectList);
 		waxCapsuleBiomassRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Biomass", ForestryItem.waxCapsuleBiomass.getItemStack(1), ForestryItem.waxCapsule.getItemStack(1), canBiomassAspectList);
 		waxCapsuleBiofuelRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Biofuel", ForestryItem.waxCapsuleBiofuel.getItemStack(1), ForestryItem.waxCapsule.getItemStack(1), canBiofuelAspectList);
+		waxCapsuleJuiceRecipe = ThaumcraftApi.addCrucibleRecipe("Transute Juice", ForestryItem.waxCapsuleJuice.getItemStack(1), ForestryItem.waxCapsule.getItemStack(1), canJuiceAspectList);
 		
 		refractoryWaterRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Water", ForestryItem.refractoryWater.getItemStack(1), ForestryItem.refractoryEmpty.getItemStack(1), canWaterAspectList);
 		refractoryLavaRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Lava", ForestryItem.refractoryLava.getItemStack(1), ForestryItem.refractoryEmpty.getItemStack(1), canLavaAspectList);
 		refractoryHoneyRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Honey", ForestryItem.refractoryHoney.getItemStack(1), ForestryItem.refractoryEmpty.getItemStack(1), canHoneyAspectList);
 		refractoryBiomassRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Biomass", ForestryItem.refractoryBiomass.getItemStack(1), ForestryItem.refractoryEmpty.getItemStack(1), canBiomassAspectList);
 		refractoryBiofuelRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Biofuel", ForestryItem.refractoryBiofuel.getItemStack(1), ForestryItem.refractoryEmpty.getItemStack(1), canBiofuelAspectList);
+		refractoryJuiceRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Juice", ForestryItem.refractoryJuice.getItemStack(1), ForestryItem.refractoryEmpty.getItemStack(1), canJuiceAspectList);
 		
 		canWaterRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Water", ForestryItem.canWater.getItemStack(1), ForestryItem.canEmpty.getItemStack(1), canWaterAspectList);
 		canLavaRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Lava", ForestryItem.canLava.getItemStack(1), ForestryItem.canEmpty.getItemStack(1), canLavaAspectList);
 		canHoneyRecipe = ThaumcraftApi.addCrucibleRecipe("Tranmute Honey", ForestryItem.canHoney.getItemStack(1), ForestryItem.canEmpty.getItemStack(1), canHoneyAspectList);
 		canBiomassRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Biomass", ForestryItem.canBiomass.getItemStack(1), ForestryItem.canEmpty.getItemStack(1), canBiomassAspectList);
 		canBiofuelRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Biofuel", ForestryItem.canBiofuel.getItemStack(1), ForestryItem.canEmpty.getItemStack(1), canBiofuelAspectList);
+		canJuiceRecipe = ThaumcraftApi.addCrucibleRecipe("Transmute Juice", ForestryItem.canJuice.getItemStack(1), ForestryItem.canEmpty.getItemStack(1), canJuiceAspectList);
 	}
 	
 	public static void thaumonomiconResearch() {
@@ -99,6 +107,10 @@ public class Forestry {
 		text = "1";
 		research = new TLResearchItem("Transmute Biofuel", "Transmute Liquids", new AspectList(), 3, 4, 3, ForestryItem.canBiofuel.getItemStack(1)).setPages(new ResearchPage(canBiofuelText), new ResearchPage(waxCapsuleBiofuelRecipe),
 				new ResearchPage(refractoryBiofuelRecipe), new ResearchPage(canBiofuelRecipe)).setConcealed().setSpecial().setParents(new String[] { "Transmute Biomass" }).registerResearchItem();
+		
+		text = "1";
+		research = new TLResearchItem("Transmute Juice", "Transmute Liquids", new AspectList(), -3, 4, 1, ForestryItem.canJuice.getItemStack(1)).setPages(new ResearchPage(canJuiceText), new ResearchPage(waxCapsuleJuiceRecipe),
+				new ResearchPage(refractoryJuiceRecipe), new ResearchPage(canJuiceRecipe)).setConcealed().setParents(new String[] { "Transmute Honey" }).registerResearchItem();
 	}
 	
 	public static void thaumonomiconResearchBuidcraft() {
@@ -124,5 +136,9 @@ public class Forestry {
 		text = "1";
 		research = new TLResearchItem("Transmute Biofuel", "Transmute Liquids", new AspectList(), 5, 4, 3, ForestryItem.canBiofuel.getItemStack(1)).setPages(new ResearchPage(canBiofuelText), new ResearchPage(waxCapsuleBiofuelRecipe),
 				new ResearchPage(refractoryBiofuelRecipe), new ResearchPage(canBiofuelRecipe)).setConcealed().setSpecial().setParents(new String[] { "Transmute Biomass" }).registerResearchItem();
+		
+		text = "1";
+		research = new TLResearchItem("Transmute Juice", "Transmute Liquids", new AspectList(), -3, 4, 1, ForestryItem.canJuice.getItemStack(1)).setPages(new ResearchPage(canJuiceText), new ResearchPage(waxCapsuleJuiceRecipe),
+				new ResearchPage(refractoryJuiceRecipe), new ResearchPage(canJuiceRecipe)).setConcealed().setParents(new String[] { "Transmute Honey" }).registerResearchItem();
 	}
 }
